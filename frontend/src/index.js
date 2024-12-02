8e6f03779dbb25f7fa5093ffb977c86f316f8a0c
KOUSHIK PINNU <pinnukoushik1@gmail.com>
2024-12-02 20:11:15 +0530

    abcd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { store } from './redux/store';
+import {Provider }from 'react-redux'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="REMOVED_CLIENT_IDl.apps.googleusercontent.com">
-    <App />
+    <Provider store={store}> 
+      <App/>
+       </Provider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
